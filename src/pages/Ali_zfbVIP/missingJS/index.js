523fdc4beacb5cd79d8c5ef34ba203c15b9430d1
syukinmei <347086009@qq.com>
2022-01-20 01:14:35 +0800

    1.20 拖动改变元素宽度实现宽度自适应大小

--- src/pages/Ali_zfbVIP/missingJS/index.js
+++ src/pages/Ali_zfbVIP/missingJS/index.js
@@ -1,37 +1,93 @@
-import React from 'react';
-import { Anchor, Row, Col } from 'antd';
+import React, { useEffect } from 'react';
+// import { Anchor, Row, Col } from 'antd';
+import './index.less';
 function miss2() {
+  useEffect(() => {
+    dragControllerDiv();
+  }, []);
+
+  const dragControllerDiv = function () {
+    var resize = document.getElementsByClassName('resize');
+    var resize2 = document.getElementsByClassName('resize2');
+    var left = document.getElementsByClassName('left');
+    var right = document.getElementsByClassName('right');
+    var mid = document.getElementsByClassName('mid');
+    var box = document.getElementsByClassName('box');
+    for (let i = 0; i < resize.length; i++) {
+      resize[i].onmousedown = function (e) {
+        var startX = e.clientX;
+        resize[i].left = resize[i].offsetLeft;
+        document.onmousemove = function (e) {
+          var endX = e.clientX;
+          var rightW = right[i].offsetWidth;
+          var moveLen = resize[i].left + (endX - startX);
+          var maxT = box[i].clientWidth - resize[i].offsetWidth;
+          if (moveLen < 150) moveLen = 150;
+          if (moveLen > maxT - rightW - 150) moveLen = maxT - rightW - 150;
+
+          resize[i].style.left = moveLen;
+
+          for (let j = 0; j < left.length; j++) {
+            left[j].style.width = moveLen + 'px';
+            mid[j].style.width =
+              box[i].clientWidth - moveLen - rightW - 10 + 'px';
+          }
+        };
+        document.onmouseup = function (evt) {
+          document.onmousemove = null;
+          document.onmouseup = null;
+          resize[i].releaseCapture && resize[i].releaseCapture();
+        };
+        resize[i].setCapture && resize[i].setCapture();
+        return false;
+      };
+    }
+    for (let i = 0; i < resize2.length; i++) {
+      resize2[i].onmousedown = function (e) {
+        var startX = e.clientX;
+        resize2[i].left = resize2[i].offsetLeft;
+        document.onmousemove = function (e) {
+          var endX = e.clientX;
+          var leftW = left[i].offsetWidth;
+          var moveLen = resize2[i].left + (endX - startX) - leftW;
+          var maxT = box[i].clientWidth - resize2[i].offsetWidth - 5;
+          if (moveLen < 150) moveLen = 150;
+          if (moveLen > maxT - leftW - 150) moveLen = maxT - leftW - 150;
+
+          resize2[i].style.left = moveLen;
+          for (let j = 0; j < right.length; j++) {
+            mid[j].style.width = moveLen + 'px';
+            right[j].style.width =
+              box[i].clientWidth - moveLen - leftW - 10 + 'px';
+          }
+        };
+        document.onmouseup = function (evt) {
+          document.onmousemove = null;
+          document.onmouseup = null;
+          resize2[i].releaseCapture && resize2[i].releaseCapture();
+        };
+        resize2[i].setCapture && resize2[i].setCapture();
+        return false;
+      };
+    }
+  };
+
   return (
-    <div
-      style={{
-        width: '1450px',
-        // height: '7000px',
-        margin: '20px auto',
-      }}
-    >
-      <Row>
-        <Col flex={10}>
-          <div
-            style={{
-              background: 'red',
-              // width: '70%',
-              height: '7000px',
-            }}
-          >
-            aa
-          </div>
-        </Col>
-        <Col flex={2}>
-          <div style={{ background: 'blue', width: '100px' }}>
-            <Anchor targetOffset={50}>
-              <Anchor.Link title="基本信息" />
-              <Anchor.Link title="四方分析" />
-              <Anchor.Link title="赌客发现" />
-              <Anchor.Link title="关联信息" />
-            </Anchor>
-          </div>
-        </Col>
-      </Row>
+    <div className="box">
+      <ul className="box">
+        <li className="left">西瓜</li>
+        <li className="resize"></li>
+        <li className="mid">备注2</li>
+        <li className="resize2"></li>
+        <li className="right">test</li>
+      </ul>
+      <ul className="box">
+        <li className="left">芒果</li>
+        <li className="resize"></li>
+        <li className="mid">备注</li>
+        <li className="resize2"></li>
+        <li className="right">test</li>
+      </ul>
     </div>
   );
 }
